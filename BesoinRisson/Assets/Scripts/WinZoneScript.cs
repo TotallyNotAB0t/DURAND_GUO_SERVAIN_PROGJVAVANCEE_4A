f9508c8ee75087e0537dd91b93ba02ab0767df6c
KindiLuv <anykennylster@gmail>
2022-09-26 15:32:59 +0100

    Winzones dones for UI

--- BesoinRisson/Assets/Scripts/WinZoneScript.cs
+++ BesoinRisson/Assets/Scripts/WinZoneScript.cs
@@ -1,26 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class WinZoneScript : MonoBehaviour
 {
     [SerializeField] private GameObject _playerConcerned;
     [SerializeField] private GameObject _uiWinScreen;
-   
-    /*
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("boop");
-        if (other.Equals(_playerConcerned.GetComponent<Collider>()))
-        {
-            Debug.Log("sapass");
-            _uiWinScreen.SetActive(true);
-        }
-    }*/
+    [SerializeField] private TextMeshProUGUI _winnerText;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log("yeet");
+        if (col.Equals(_playerConcerned.GetComponent<Collider2D>())) 
+        {
+            Debug.Log("sapass");
+            _winnerText.text = $"{_playerConcerned.name} won!";
+            _uiWinScreen.SetActive(true);
+        }
     }
 }
