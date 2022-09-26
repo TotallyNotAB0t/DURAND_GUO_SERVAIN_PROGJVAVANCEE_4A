b20e1a419a26fc99db3d6e49f88075b453d8f9d6
KindiLuv <anykennylster@gmail>
2022-09-26 14:46:32 +0100

    bug trigger

---
+++ BesoinRisson/Assets/Scripts/WinZoneScript.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinZoneScript : MonoBehaviour
+{
+    [SerializeField] private GameObject _playerConcerned;
+    [SerializeField] private GameObject _uiWinScreen;
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("boop");
+        if (other.Equals(_playerConcerned.GetComponent<Collider>()))
+        {
+            Debug.Log("sapass");
+            _uiWinScreen.SetActive(true);
+        }
+    }
+}
