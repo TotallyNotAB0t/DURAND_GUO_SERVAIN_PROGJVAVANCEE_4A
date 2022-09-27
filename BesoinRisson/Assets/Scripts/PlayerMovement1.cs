2d28de7f778ae8a2f805b797ab9dcb8e2f6157ce
Pierre Durand <pierre.durand95360@gmail.com>
2022-09-27 21:03:22 +0200

    Merge Sword into master

--- BesoinRisson/Assets/Scripts/PlayerMovement1.cs
+++ BesoinRisson/Assets/Scripts/PlayerMovement1.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections;
 using Enums;
 using Extensions;
 using Interfaces;
--- BesoinRisson/Assets/Scripts/PlayerMovement2.cs
+++ BesoinRisson/Assets/Scripts/PlayerMovement2.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using Enums;
 using Extensions;
 using Interfaces;
