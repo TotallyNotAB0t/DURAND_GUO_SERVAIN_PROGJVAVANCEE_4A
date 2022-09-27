273a23d661fb3e482ce6b03ae850138d5614322d
KindiLuv <anykennylster@gmail>
2022-09-27 18:04:30 +0100

    added settings

---
+++ BesoinRisson/Assets/Scripts/SettingsController.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+using Slider = UnityEngine.UI.Slider;
+
+public class SettingsController : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private TMP_Dropdown _resolutionDropdown;
+    [SerializeField] private Slider _volumeSlider;
+    private float _currentVolume;
+    private Resolution[] _resolutions;
+
+    private void Start()
+    {
+        _resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        _resolutions = Screen.resolutions;
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < _resolutions.Length; i++)
+        {
+            string option = _resolutions[i].width + "x" + _resolutions[i].height;
+            options.Add(option);
+            if (_resolutions[i].width == Screen.currentResolution.width 
+                && _resolutions[i].height == Screen.currentResolution.height)
+                currentResolutionIndex = i;
+        }
+        
+        _resolutionDropdown.AddOptions(options);
+        _resolutionDropdown.RefreshShownValue();
+        LoadSettings(currentResolutionIndex);
+    }
+
+    public void SetVolume(float volume)
+    {
+        _audioMixer.SetFloat("Volume", volume);
+        _currentVolume = volume;
+    }
+
+    public void SetFullScreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+    public void SetResolution()
+    {
+        Debug.Log(_resolutionDropdown.value);
+        Resolution resolution = _resolutions[_resolutionDropdown.value];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("ResolutionPreference", 
+            _resolutionDropdown.value);
+        PlayerPrefs.SetInt("FullscreenPreference", 
+            Convert.ToInt32(Screen.fullScreen));
+        PlayerPrefs.SetFloat("VolumePreference", 
+            _currentVolume); 
+    }
+    
+    public void LoadSettings(int currentResolutionIndex)
+    {
+        if (PlayerPrefs.HasKey("ResolutionPreference"))
+            _resolutionDropdown.value = 
+                PlayerPrefs.GetInt("ResolutionPreference");
+        else
+            _resolutionDropdown.value = currentResolutionIndex;
+        if (PlayerPrefs.HasKey("FullscreenPreference"))
+            Screen.fullScreen = 
+                Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPreference"));
+        else
+            Screen.fullScreen = true;
+        if (PlayerPrefs.HasKey("VolumePreference"))
+            _volumeSlider.value = 
+                PlayerPrefs.GetFloat("VolumePreference");
+        else
+            _volumeSlider.value = 
+                PlayerPrefs.GetFloat("VolumePreference");
+    }
+}
