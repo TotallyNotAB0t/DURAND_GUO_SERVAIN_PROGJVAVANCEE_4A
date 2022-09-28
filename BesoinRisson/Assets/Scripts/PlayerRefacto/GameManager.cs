ba9393fde6fb001dea0fddd67fc9bf5c187f62ac
KindiLuv <anykennylster@gmail>
2022-09-28 14:57:07 +0100

    started with state and manager

---
+++ BesoinRisson/Assets/Scripts/PlayerRefacto/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace PlayerRefacto
+{
+    public class GameManager : MonoBehaviour
+    {
+        // ici c'est la logique de jeu, tu lui passes un gamestate et un input
+    }
+}
