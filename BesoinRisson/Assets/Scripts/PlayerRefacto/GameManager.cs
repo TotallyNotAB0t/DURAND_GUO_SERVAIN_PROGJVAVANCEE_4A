001affb1bbd26d0f8dbb43349b419f9e373eb0c1
Pierre Durand <pierre.durand95360@gmail.com>
2022-09-29 14:38:16 +0200

    gravity wip

--- BesoinRisson/Assets/Scripts/PlayerRefacto/GameManager.cs
+++ BesoinRisson/Assets/Scripts/PlayerRefacto/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Behaviour.InputSystems;
+using Behaviour.Utils;
 using Enums;
 using Interfaces;
 using UnityEngine;
@@ -12,6 +13,7 @@ namespace PlayerRefacto
         private PlayerInputs _inputProvider;
         private PlayerInputs1 _inputProvider1;
         private GameState state;
+        private GameState GMBuffer;
         [SerializeField] private GameObject player1;
         [SerializeField] private GameObject player2;
 
@@ -26,16 +28,67 @@ namespace PlayerRefacto
         
         private void Update()
         {
-            //Debug.Log($"p1 pos :{player1.transform.position}; p1 new vector : {state.p1.pos}");
+            //Create Copy GameState for checking
+            GMBuffer = state;
+            
+            //Left and right move
             ReadInputLeftOrRight();
             player1.transform.position = Vector2.MoveTowards(player1.transform.position, state.p1.pos, Time.deltaTime*5);
+            player2.transform.position = Vector2.MoveTowards(player2.transform.position, state.p2.pos, Time.deltaTime*5);
+            
+            //Gravity
+
+            MyUpdate();
+            
+            player1.transform.position = state.p1.pos;
+        }
+
+        public void MyUpdate()
+        {
+            Debug.LogError("pos p1 : "+state.p1.pos);
+            if (!CheckGround(state.p1.pos, 0.5f, new Vector2(0, 0.5f), 1))
+            {
+                Debug.LogError("not on ground");
+                GMBuffer.p1.pos = ApplyGravity(GMBuffer.p1.pos);
+            }
+
+            state.p1.pos = Vector2.MoveTowards(state.p1.pos, GMBuffer.p1.pos, 0.016f);
+            Debug.LogError("DESCEND : "+state.p1.pos);
+        }
+
+        //Input function
+        public void ReadInputLeftOrRight()
+        {
+            if (_inputProvider.GetActionPressed(InputAction.Left))
+            {
+                PlayerGoLeft(true);
+            }
+            if (_inputProvider1.GetActionPressed(InputAction.Left1))
+            {
+                PlayerGoLeft(false);
+            }
+            
+            if (_inputProvider.GetActionPressed(InputAction.Right))
+            {
+                PlayerGoRight(true);
+            }
+            if (_inputProvider1.GetActionPressed(InputAction.Right1))
+            {
+                PlayerGoRight(false);
+            }
         }
         
         //Moving function
-
-        //Input function
-  
+        public void PlayerGoLeft(bool isP1)
+        {
+            state.ModifyPos(isP1, Vector2.left * 0.1f);
+        }
         
+        public void PlayerGoRight(bool isP1)
+        {
+            state.ModifyPos(isP1, Vector2.right * 0.1f);
+        }
+
         //ici c'est du game manager
         /*public void readInputs(InputAction input, GameState.Player p1)
         {
@@ -99,37 +152,6 @@ namespace PlayerRefacto
             }
         }
 
-        public void PlayerGoLeft(bool isP1)
-        {
-            state.ModifyPos(isP1, Vector2.left * 0.1f);
-        }
-        
-        public void PlayerGoRight(bool isP1)
-        {
-            state.ModifyPos(isP1, Vector2.right * 0.1f);
-        }
-        
-        public void ReadInputLeftOrRight()
-        {
-            if (_inputProvider.GetActionPressed(InputAction.Left))
-            {
-                PlayerGoLeft(true);
-            }
-            if (_inputProvider.GetActionPressed(InputAction.Left1))
-            {
-                PlayerGoLeft(false);
-            }
-            
-            if (_inputProvider.GetActionPressed(InputAction.Right))
-            {
-                PlayerGoRight(true);
-            }
-            if (_inputProvider.GetActionPressed(InputAction.Right1))
-            {
-                PlayerGoRight(false);
-            }
-        }
-
         /*private void applyStab(GameState.Player player, GameState.Player rival)
         {
             player.isAttacking = true;
@@ -161,6 +183,21 @@ namespace PlayerRefacto
         public bool CheckOverlap(Vector2 collider1, float radius1, Vector2 collider2, float radius2)
         {
             return Vector2.Distance(collider1, collider2) < (radius1/2)+(radius2/2);
+        }
+        
+        public bool Check(Vector2 collider1, float radius1, Vector2 collider2, float radius2)
+        {
+            return Vector2.Distance(collider1, collider2) < (radius1/2)+(radius2/2);
+        }
+        
+        public bool CheckGround(Vector2 collider1, float radius1, Vector2 ground, float size)
+        {
+            return Vector2.Distance(collider1, new Vector2(collider1.x, ground.y)) < (radius1/2)+(size/2);
+        }
+
+        private Vector2 ApplyGravity(Vector2 player)
+        {
+            return player + Vector2.down * 9.81f;
         }
     }
 }
